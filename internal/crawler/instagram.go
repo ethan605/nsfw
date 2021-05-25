86f51eda3aa76c5987afe599a63e1318d571352a
Ethan Nguyen <ethan.nguyen@onfido.com>
2021-05-25 22:15:06 +0100

    Add comment documents

--- internal/crawler/instagram.go
+++ internal/crawler/instagram.go
@@ -19,6 +19,7 @@ var (
 	UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"
 )
 
+// NewInstagramProfile creates a profile of an Instagram user
 func NewInstagramProfile(data map[string]interface{}) Profile {
 	avatarURL, _ := data["AvatarURL"].(string)
 	displayName, _ := data["DisplayName"].(string)
