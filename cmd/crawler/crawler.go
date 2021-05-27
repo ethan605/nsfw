80154eb0d7f347341a3bf1c10acbf27f2d178e0b
Ethanify <thanhnx.605@gmail.com>
2021-05-27 17:08:29 +0100

    Add expGoroutines

--- cmd/crawler/crawler.go
+++ cmd/crawler/crawler.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"nsfw/internal/crawler"
+	"runtime"
+	"time"
 )
 
 func main() {
-	crawlInstagram(false)
+	crawlInstagram(true)
+	expGoroutines()
 }
 
 /* Private stuffs */
@@ -45,4 +48,38 @@ func panicOnError(err error) {
 	if err != nil {
 		log.Panicln(err)
 	}
+}
+
+func expGoroutines() {
+	queue := make(chan int, 5)
+	// done := make(chan struct{})
+	timer := time.After(3 * time.Second)
+
+	go func() {
+		num := 0
+
+		for {
+			select {
+			case queue <- num:
+				log.Println("enqueue:", num)
+				time.Sleep(100 * time.Millisecond)
+				num++
+			case <-timer:
+				log.Println("Break signal received")
+				close(queue)
+				return
+			}
+		}
+	}()
+
+	for num := range queue {
+		go func(num int) {
+			log.Println("  - processing:", num, len(queue))
+			time.Sleep(2 * time.Second)
+			log.Println("  - done:", num, len(queue))
+		}(num)
+		time.Sleep(time.Second)
+	}
+
+	log.Println("Exitting. Goroutines:", runtime.NumGoroutine())
 }
